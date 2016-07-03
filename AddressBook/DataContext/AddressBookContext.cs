b588e9c982f3c5f64eda77d2c0948d9d2f8f2c82
dherrin792 <dherrin79@gmail.com>
2016-07-03 01:41:44 -0600

    Added DataContext Layer

---
+++ AddressBook/DataContext/AddressBookContext.cs
@@ -0,0 +1,20 @@
+﻿using DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContext
+{
+    class AddressBookContext : DbContext
+    {
+        public AddressBookContext() : base("AddressBook")
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
